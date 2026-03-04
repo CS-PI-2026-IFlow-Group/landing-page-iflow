c187d1c08ce89865f1285c8d00b511fd7a7835ee
Ana Beatriz Tavares Malaquais <ana.beat20051@gmail.com>
2026-03-04 17:43:38 -0300

    feat: estilização do modo de alto constraste

--- js/main.js
+++ js/main.js
@@ -14,3 +14,35 @@ overlay.addEventListener("click", toggleMenu);
 document.querySelectorAll(".nav-link").forEach((link) => {
   link.addEventListener("click", toggleMenu);
 });
+
+let fontSize = 16;
+const htmlElement = document.documentElement;
+
+document.getElementById("aumentar-fonte").addEventListener("click", () => {
+  if (fontSize < 24) {
+    fontSize += 2;
+    htmlElement.style.fontSize = fontSize + "px";
+  }
+});
+
+document.getElementById("diminuir-fonte").addEventListener("click", () => {
+  if (fontSize > 12) {
+    fontSize -= 2;
+    htmlElement.style.fontSize = fontSize + "px";
+  }
+});
+
+const btnContraste = document.getElementById("alto-contraste");
+btnContraste.addEventListener("click", () => {
+  document.body.classList.toggle("alto-contraste");
+
+  const isContraste = document.body.classList.contains("alto-contraste");
+  localStorage.setItem("alto-contraste", isContraste);
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  const contrastSaved = localStorage.getItem("alto-contraste");
+  if (contrastSaved === "true") {
+    document.body.classList.add("alto-contraste");
+  }
+});
