01e28f791a390ae03df1fe12856afa5c33e2846f
Ana Malaquias <ana.beat20051@gmail.com>
2026-03-13 22:39:44 -0300

    fix: corrige posição da div de acessibilidade para evitar que fique em cima de outros conteúdos
    - esconde acessbilidade para melhorar navegação em telas menores

--- js/main.js
+++ js/main.js
@@ -300,88 +300,76 @@ fetch("json/faq.json")
             if (details !== outro) {
               outro.removeAttribute("open");
             }
+          });
         }
-        )
-    }
-
-
-
-})
-
-
-});
-
-
-
-
-
-
-});
+      });
+    });
+  });
 
 const quemSomos = document.querySelector(".container-membros");
 
 fetch("json/membros.json")
-    .then(resposta => resposta.json())
-    .then(membrosLista => {
-        membrosLista.forEach(item => {
-        const nomeMembro = document.createElement("h1");
-        const cargo = document.createElement("p");
-        const descricaoCargo = document.createElement("h3");
-        const foto = document.createElement("img");
-        const membroUnico = document.createElement("div");
+  .then((resposta) => resposta.json())
+  .then((membrosLista) => {
+    membrosLista.forEach((item) => {
+      const nomeMembro = document.createElement("h1");
+      const cargo = document.createElement("p");
+      const descricaoCargo = document.createElement("h3");
+      const foto = document.createElement("img");
+      const membroUnico = document.createElement("div");
 
-        const botaoGit = document.createElement("a");
-        const botaoLinkedin = document.createElement("a");
-        const iconeGit = document.createElement("img");
-        const iconeLinkedin = document.createElement("img");
-        const contatoPessoal = document.createElement("div");
+      const botaoGit = document.createElement("a");
+      const botaoLinkedin = document.createElement("a");
+      const iconeGit = document.createElement("img");
+      const iconeLinkedin = document.createElement("img");
+      const contatoPessoal = document.createElement("div");
 
+      membroUnico.classList.add("membro-unico-container");
 
-        membroUnico.classList.add("membro-unico-container");
+      contatoPessoal.classList.add("contato-pessoal");
 
-        contatoPessoal.classList.add("contato-pessoal");
+      foto.src = item.foto;
+      foto.alt = item.descricaoFoto;
+      foto.classList.add("foto-membro");
 
-        foto.src = item.foto;
-        foto.alt = item.descricaoFoto;
-        foto.classList.add("foto-membro");
+      iconeGit.src = "assets/svg/github.svg";
+      iconeGit.alt = "ícone do GitHub";
 
+      iconeLinkedin.src = "assets/svg/linkedin.svg";
+      iconeLinkedin.alt = "ícone do LinkedIn";
 
-        iconeGit.src = "assets/svg/github.svg";
-        iconeGit.alt = "ícone do GitHub"
+      nomeMembro.textContent = item.nome;
+      cargo.textContent = item.cargo;
+      descricaoCargo.textContent = item.descricaoCargo;
 
-        iconeLinkedin.src = "assets/svg/linkedin.svg";
-        iconeLinkedin.alt = "ícone do LinkedIn";
+      botaoGit.href = item.linkGit;
+      botaoGit.target = "_blank";
 
+      botaoLinkedin.href = item.botaoLinkedin;
+      botaoLinkedin.target = "_blank";
 
+      botaoLinkedin.appendChild(iconeLinkedin);
+      botaoGit.appendChild(iconeGit);
+      contatoPessoal.appendChild(botaoGit);
+      contatoPessoal.appendChild(botaoLinkedin);
 
-        nomeMembro.textContent = item.nome;
-        cargo.textContent = item.cargo;
-        descricaoCargo.textContent = item.descricaoCargo;
+      membroUnico.appendChild(foto);
+      membroUnico.appendChild(nomeMembro);
+      membroUnico.appendChild(cargo);
+      membroUnico.appendChild(descricaoCargo);
+      membroUnico.appendChild(contatoPessoal);
 
-        botaoGit.href = item.linkGit;
-        botaoGit.target = "_blank";
+      quemSomos.appendChild(membroUnico);
+    });
+  });
 
-        botaoLinkedin.href = item.botaoLinkedin
-        botaoLinkedin.target = "_blank";
+const menuAcessibilidade = document.getElementById("menu-acessibilidade");
+const btnToggleAcessibilidade = document.getElementById(
+  "btn-toggle-acessibilidade",
+);
 
-
-        botaoLinkedin.appendChild(iconeLinkedin);
-        botaoGit.appendChild(iconeGit);
-        contatoPessoal.appendChild(botaoGit);
-        contatoPessoal.appendChild(botaoLinkedin);
-
-        membroUnico.appendChild(foto);
-        membroUnico.appendChild(nomeMembro);
-        membroUnico.appendChild(cargo);
-        membroUnico.appendChild(descricaoCargo);
-        membroUnico.appendChild(contatoPessoal);
-
-        quemSomos.appendChild(membroUnico);
-
-
-        })
-
-
-
-
-    })
+if (btnToggleAcessibilidade && menuAcessibilidade) {
+  btnToggleAcessibilidade.addEventListener("click", () => {
+    menuAcessibilidade.classList.toggle("aberta");
+  });
+}
